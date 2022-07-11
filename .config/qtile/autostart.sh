0a5a5a335adad0ea17280bbd964fb119d2c199f6
santorar <estractor179@gmail.com>
2022-07-10 23:14:42 -0500

    added my laptop config of qtile

---
+++ .config/qtile/autostart.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+nitrogen --restore &
+setxkbmap -layout us -variant altgr-intl &
+picom --experimental-backends &
+nm-applet &
+xset r rate 200 30 &
+flameshot &
+cbatticon &
--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -24,13 +24,27 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from libqtile import bar, layout, widget
+from libqtile import bar, layout, widget,hook
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
+import os
+import subprocess
 
 mod = "mod4"
-terminal = guess_terminal()
+terminal = "kitty"
+browser = "brave"
+explorer = "nemo"
+POWERLINE_SYMBOL = "\ue0ba"
+
+COLORS = {
+        "black" : "#17070a",
+        "white" : "#D9E0EE",
+        "purple" : "#F28FAD",
+        "yellow" : "#FAE3B0",
+        "pink" : "#96CDFB",
+        }
+
 
 keys = [
     # A list of available commands that can be bound to keys can be found
@@ -40,13 +54,15 @@ keys = [
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+    Key([mod, "shift"], "space", lazy.layout.next(), desc="Move window focus to other window"),
+
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
     Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
     Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
     Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
@@ -54,6 +70,7 @@ keys = [
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
@@ -67,83 +84,188 @@ keys = [
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
+    Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    Key([mod], "b", lazy.spawn(browser), desc="Launch browser"),
+    Key([mod], "e", lazy.spawn(explorer), desc="Launch explorer"),
+    Key([mod], "space", lazy.spawn("rofi -show drun"), desc="Launch rofi"),
+    Key([mod], "c", lazy.spawn("gnome-calculator"), desc="Launch a calculator"),
+    Key([mod, "shift","control"], "l", lazy.spawn("i3lock-fancy"), desc="Locks the screen"),
+
+
+    # ------------ Hardware Configs ------------
+
+    # Volume
+    Key([], "XF86AudioLowerVolume", lazy.spawn(
+        "pactl set-sink-volume @DEFAULT_SINK@ -5%"
+    )),
+    Key([], "XF86AudioRaiseVolume", lazy.spawn(
+        "pactl set-sink-volume @DEFAULT_SINK@ +5%"
+    )),
+    Key([], "XF86AudioMute", lazy.spawn(
+        "pactl set-sink-mute @DEFAULT_SINK@ toggle"
+    )),
+
+    # Brightness
+    Key([], "XF86MonBrightnessUp", lazy.spawn("brightnessctl set +10%")),
+    Key([], "XF86MonBrightnessDown", lazy.spawn("brightnessctl set 10%-")),
+
+    #switch monitors
+    Key([mod], "period", lazy.next_screen(), desc="Move focus to next monitor"),
+    Key([mod], "comma", lazy.prev_screen(), desc="Move focus to previous monitor"),
+
+    #tile and foat windows
+    Key([mod, "shift"], "f", lazy.window.toggle_floating(), desc="toggle floating on a window"),
+    Key([mod], "f", lazy.window.toggle_fullscreen(), desc="toggle fullscreen on a window"),
 ]
 
-groups = [Group(i) for i in "123456789"]
+#groups = [Group(i) for i in "123456789"]
 
-for i in groups:
+def longNameParse(text): 
+    for string in ["NVIM","Brave","Google Chrome", "Firefox"]: 
+        if string in text:
+            text = string
+        else:
+            text = text
+    return text
+
+groups = [
+        Group(name, **kwargs)
+        for name, kwargs in[
+            ("일", {"matches": [Match(wm_class=["firefox"])]}),
+            ("이", {}),
+            ("삼", {}),
+            ("사", {}),
+            ("오", {}),
+            ("육", {"matches": [Match(wm_class=["spotify"])]}),
+            ("칠", {"matches": [Match(wm_class=["mpv"])]}),
+            ("팔", {"matches": [Match(wm_class=["discord"])]}),
+            ]
+        ]
+
+for i, group in enumerate(groups, 1):
     keys.extend(
         [
-            # mod1 + letter of group = switch to group
+            ###
+            ### Switch between workspaces
+            ###
             Key(
                 [mod],
-                i.name,
-                lazy.group[i.name].toscreen(),
-                desc="Switch to group {}".format(i.name),
+                str(i if i != 10 else 0),
+                lazy.group[group.name].toscreen(toggle=False),
+                desc=f"Switch to group {group.name}",
             ),
-            # mod1 + shift + letter of group = switch to & move focused window to group
+            ###
+            ### Move focused window to workspace
+            ###
             Key(
                 [mod, "shift"],
-                i.name,
-                lazy.window.togroup(i.name, switch_group=True),
-                desc="Switch to & move focused window to group {}".format(i.name),
+                str(i if i != 10 else 0),
+                lazy.window.togroup(group.name),
+                desc=f"Move focused window to group {group.name}",
             ),
-            # Or, use below if you prefer not to switch to that group.
-            # # mod1 + shift + letter of group = move focused window to group
-            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-            #     desc="move focused window to group {}".format(i.name)),
         ]
     )
 
 layouts = [
-    layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
+        layout.Bsp(border_width=2,margin = 3,margin_on_single=False,border_focus=COLORS["pink"],),
+    # layout.Columns(border_focus_stack=[COLORS["purple"], COLORS["purple"]], border_width=2,margin = 3),
     layout.Max(),
     # Try more layouts by unleashing below layouts.
     # layout.Stack(num_stacks=2),
-    # layout.Bsp(),
     # layout.Matrix(),
     # layout.MonadTall(),
-    # layout.MonadWide(),
+    # layout.MonadWide(),pink
     # layout.RatioTile(),
     # layout.Tile(),
     # layout.TreeTab(),
-    # layout.VerticalTile(),
-    # layout.Zoomy(),
+    # layout.VerticalTile(), 
 ]
-
 widget_defaults = dict(
-    font="sans",
-    fontsize=12,
-    padding=3,
+    font = 'Iosevka Fixed Semibold',
+    fontsize=13,
+    padding=0,
 )
 extension_defaults = widget_defaults.copy()
 
 screens = [
     Screen(
-        bottom=bar.Bar(
+        top=bar.Bar(
             [
-                widget.CurrentLayout(),
-                widget.GroupBox(),
-                widget.Prompt(),
-                widget.WindowName(),
-                widget.Chord(
-                    chords_colors={
-                        "launch": ("#ff0000", "#ffffff"),
-                    },
-                    name_transform=lambda name: name.upper(),
+                widget.Spacer(length = 5),
+                widget.GroupBox(
+                    font="Ubuntu Mono",
+                    highlight_method='text',
+                    highlight_color=COLORS["black"],
+                    active = COLORS["white"],
+                    this_current_screen_border=COLORS["yellow"],
+                    other_screen_border=COLORS["pink"],
+                    padding = 5,
+                    fontsize=13,
                 ),
-                widget.TextBox("default config", name="default"),
-                widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
-                widget.Systray(),
-                widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
-                widget.QuickExit(),
+                widget.TextBox(
+                    text=POWERLINE_SYMBOL,
+                    foreground = COLORS["white"],
+                    background = COLORS["black"],
+                    fontsize=30,
+                ),
+                widget.WindowName(
+                    foreground=COLORS["black"],
+                    parse_text=longNameParse,
+                    padding=5,
+                    background=COLORS["white"],
+                    ),
+                widget.TextBox(
+                    text=POWERLINE_SYMBOL,
+                    foreground = COLORS["black"],
+                    background = COLORS["white"],
+                    fontsize=30,
+                ),
+                widget.Systray(icon_size=15,padding=4,),
+                widget.TextBox(
+                    text=POWERLINE_SYMBOL,
+                    foreground = COLORS["white"],
+                    background = COLORS["black"],
+                    fontsize=30,
+                ),
+                widget.Memory(
+                    background = COLORS["white"],
+                    foreground = COLORS["black"],
+                    format = 'Ram: {MemUsed: .0f}{mm}',
+                    ),
+                widget.TextBox(
+                    text=POWERLINE_SYMBOL,
+                    foreground = COLORS["pink"],
+                    background = COLORS["white"],
+                    fontsize=30,
+                ),
+                widget.ThermalSensor(
+                    fmt = 'Thermal:  {}',
+                    background = COLORS["pink"],
+                    foreground=COLORS["black"],
+                    fontsize=13,
+                    padding=5,
+                    ),
+                widget.TextBox(
+                    text=POWERLINE_SYMBOL,
+                    foreground = COLORS["purple"],
+                    background = COLORS["pink"],
+                    fontsize=30,
+                ),
+                widget.CPU(format="CPU {load_percent}%",background = COLORS["purple"],foreground = COLORS["black"],padding=3,),
+                widget.TextBox(
+                    text=POWERLINE_SYMBOL,
+                    foreground = COLORS["yellow"],
+                    background = COLORS["purple"],
+                    fontsize=30,
+                ),
+                widget.Clock(format="%b %d - %I:%M %p",foreground = COLORS["black"], background = COLORS["yellow"],padding = 8),
             ],
-            24,
-            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
+            32,
+            background = COLORS["black"],
+            #border_width=[3, 2, 3, 2],  # Draw top and bottom borders
             # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
         ),
     ),
@@ -169,13 +291,22 @@ floating_layout = layout.Floating(
         Match(wm_class="makebranch"),  # gitk
         Match(wm_class="maketag"),  # gitk
         Match(wm_class="ssh-askpass"),  # ssh-askpass
+        Match(wm_class="gnome-calculator"),  # calculator
+        Match(wm_class="solanum"),  # pomodoro timer
         Match(title="branchdialog"),  # gitk
         Match(title="pinentry"),  # GPG key password entry
+        Match(title="gnome-calculator"),  # gnome calculator
     ]
 )
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
+
+@hook.subscribe.startup_once
+def autostart():
+        home = os.path.expanduser('/home/santorar/.config/qtile/autostart.sh')
+        subprocess.run([home])
+
 
 # If things like steam games want to auto-minimize themselves when losing
 # focus, should we respect this or not?
