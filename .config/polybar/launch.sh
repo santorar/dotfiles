1eb4aba13d447786119ac4fb364a8fc530ed113c
santorar <estractor179@gmail.com>
2023-04-23 23:00:27 -0500

    added polybar configuration

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use
+# polybar-msg cmd quit
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar, using default config location ~/.config/polybar/config.ini
+polybar top & disown
+
+
+echo "Polybar launched..."
